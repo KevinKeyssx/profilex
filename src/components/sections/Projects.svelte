14a376b7f4b52cfb54aa31c18c3329c673632e8c
kevinkeyssx <keyss1700@gmail.com>
2025-04-15 02:09:50 -0400

    Loading added

--- src/components/sections/Projects.svelte
+++ src/components/sections/Projects.svelte
@@ -5,6 +5,8 @@
     import ProjectCard      from "$components/cards/ProjectCard.svelte";
     import Search           from "$components/inputs/Search.svelte";
     import type { Project } from "$models/project";
+    import Loader           from "$components/loaders/ProjectLoader.svelte";
+    import LoaderBadge      from "$components/loaders/BadgeLoader.svelte";
 
 
     export let sectionRefs: Record<string, HTMLElement>;
@@ -15,6 +17,7 @@
     let allTechnologies: string[] = [];
     let filteredProjects = [...projects];
     let highlightedTech: string | null = null;
+    let isLoading = true;
 
 
     function handleSearchInput( event: Event ) {
@@ -79,6 +82,8 @@
             allTechnologies = Array.from( techSet ).sort();
         } catch (error) {
             console.error('Error fetching projects:', error);
+        } finally {
+            isLoading = false;
         }
     });
 </script>
@@ -90,7 +95,13 @@
 >
     <div class="container mx-auto px-4 space-y-10">
         <div class="text-center" in:fade={{ duration: 800 }}>
-            <h2 class="text-4xl font-bold mb-4">My Projects ({projects.length})</h2>
+            <h2 class="text-4xl font-bold mb-4">My Projects
+                {#if isLoading}
+                    (<svg class="inline z-10" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><g fill="none" stroke="#ca84c7" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path stroke-dasharray="16" stroke-dashoffset="16" d="M12 3c4.97 0 9 4.03 9 9"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.3s" values="16;0"/><animateTransform attributeName="transform" dur="1.5s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"/></path><path stroke-dasharray="64" stroke-dashoffset="64" stroke-opacity="0.3" d="M12 3c4.97 0 9 4.03 9 9c0 4.97 -4.03 9 -9 9c-4.97 0 -9 -4.03 -9 -9c0 -4.97 4.03 -9 9 -9Z"><animate fill="freeze" attributeName="stroke-dashoffset" dur="1.2s" values="64;0"/></path></g></svg>)
+                {:else}
+                    ({projects.length})
+                {/if}
+            </h2>
 
             <div class="w-20 h-1 bg-purple-500 mx-auto mb-8"></div>
 
@@ -113,6 +124,11 @@
 
             <!-- Technology filters -->
             <div class="flex flex-wrap gap-2">
+                {#if isLoading}
+                    {#each Array(20) as _}
+                        <LoaderBadge />
+                    {/each}
+                {:else}
                 {#each allTechnologies as tech}
                     <button 
                         class={`px-3 py-1 rounded-full text-sm transition-all ${highlightedTech === tech ? 'bg-purple-600 text-white' : 'bg-gray-800 text-gray-300 hover:bg-gray-700'}`}
@@ -121,6 +137,8 @@
                         {tech}
                     </button>
                 {/each}
+                {/if}
+                
 
                 {#if searchTerm}
                     <button 
@@ -135,13 +153,19 @@
 
         <!-- Projects Grid -->
         <div class="grid md:grid-cols-2 xl:grid-cols-3 gap-5 md:gap-5 xl:gap-6 2xl:gap-8">
-            {#each filteredProjects as project, i (project.id)}
-                <ProjectCard {project} />
+            {#if isLoading}
+                {#each [1, 2 , 3, 4 , 5 ,6] as _}
+                    <Loader />
+                {/each}
             {:else}
-                <div class="col-span-full text-center py-12">
-                    <p class="text-xl text-gray-400">No projects found matching your criteria.</p>
-                </div>
-            {/each}
+                {#each filteredProjects as project, i (project.id)}
+                    <ProjectCard {project} />
+                {:else}
+                    <div class="col-span-full text-center py-12">
+                        <p class="text-xl text-gray-400">No projects found matching your criteria.</p>
+                    </div>
+                {/each}
+            {/if}
         </div>
     </div>
 </section>
